05462540ce81e941a6f618da99a09bee6b576911
Carolyn Tran <carolyntran953@gmail.com>
2021-04-29 22:11:41 -0500

    refactor connection.js with own login info

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+const config = require('../../config.js');
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASS || config.PASSWORD,
   database: process.env.DB_NAME || 'fec_pathfinder_metadata'
 });
 
