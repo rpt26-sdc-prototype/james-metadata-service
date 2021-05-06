01bb0a9f2b57306707f5e9c7a91d1ab9bbe0c03d
Carolyn Tran <carolyntran953@gmail.com>
2021-05-06 01:22:13 -0500

    start new seeding script

--- server/newSeed.js
+++ server/newSeed.js
@@ -58,24 +58,24 @@ dbManager.initializeDatabase().then(() => {
   //Generate guaranteed product
   console.log('Generating Products...');
 
-  products.push(new SteamProduct({
-    name: "Age of Empires II: Definitive Edition",
-    price: 19.99,
-    description: `<p>Age of Empires II: Definitive Edition celebrates the 20th anniversary of one of the most popular strategy games ever with stunning 4K Ultra HD graphics, a new and fully remastered soundtrack, and brand-new content, “The Last Khans” with 3 new campaigns and 4 new civilizations.</p>
+  // products.push(new SteamProduct({
+  //   name: "Age of Empires II: Definitive Edition",
+  //   price: 19.99,
+  //   description: `<p>Age of Empires II: Definitive Edition celebrates the 20th anniversary of one of the most popular strategy games ever with stunning 4K Ultra HD graphics, a new and fully remastered soundtrack, and brand-new content, “The Last Khans” with 3 new campaigns and 4 new civilizations.</p>
 
-    <p>Explore all the original campaigns like never before as well as the best-selling expansions, spanning over 200 hours of gameplay and 1,000 years of human history. Head online to challenge other players with 35 different civilizations in your quest for world domination throughout the ages.</p>
+  //   <p>Explore all the original campaigns like never before as well as the best-selling expansions, spanning over 200 hours of gameplay and 1,000 years of human history. Head online to challenge other players with 35 different civilizations in your quest for world domination throughout the ages.</p>
 
-    <p>Choose your path to greatness with this definitive remaster to one of the most beloved strategy games of all time.</p>
+  //   <p>Choose your path to greatness with this definitive remaster to one of the most beloved strategy games of all time.</p>
 
-    <a href="https://privacy.microsoft.com/privacystatement">https://privacy.microsoft.com/privacystatement</a>`,
-    shortDescription: `Age of Empires II: Definitive Edition celebrates the 20th anniversary of one of the most popular strategy games ever with stunning 4K Ultra HD graphics, a new and fully remastered soundtrack, and brand-new content, “The Last Khans” with 3 new campaigns and 4 new civilizations.`,
-    developer: 'Forgotton Empires',
-    publisher: 'Xbox Game Studios',
-    releaseDate: new Date('14 Nov 2019').getTime(),
-    genres: [genres[0]]
-  }));
+  //   <a href="https://privacy.microsoft.com/privacystatement">https://privacy.microsoft.com/privacystatement</a>`,
+  //   shortDescription: `Age of Empires II: Definitive Edition celebrates the 20th anniversary of one of the most popular strategy games ever with stunning 4K Ultra HD graphics, a new and fully remastered soundtrack, and brand-new content, “The Last Khans” with 3 new campaigns and 4 new civilizations.`,
+  //   developer: 'Forgotton Empires',
+  //   publisher: 'Xbox Game Studios',
+  //   releaseDate: new Date('14 Nov 2019').getTime(),
+  //   genres: [genres[0]]
+  // }));
   //generate 99 fake games
-  for (var i = 0; i < 99; i++) {
+  for (var i = 0; i < 1000; i++) {
     var options = {genres: []};
     //Generate random genres
     for (var j = 0; j < Math.floor(Math.random() * 4) + 1; j++) {
